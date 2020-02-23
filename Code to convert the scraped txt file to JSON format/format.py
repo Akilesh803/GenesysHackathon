dfb1ef3923abcea79a9db66e0701a993fb1eda39
iamajmalhassan <iamajmalhassan@gmail.com>
2020-02-23 11:23:27 +0530

    Add files via upload

---
+++ Code to convert the scraped txt file to JSON format/format.py
@@ -0,0 +1,43 @@
+name = "acer aspire 5"
+
+f1 = open(name+".txt",'r')
+f2 = open(name+" json.json",'w')
+
+f2.write("[\n")
+n = 0
+f11 = f1.readlines()
+for row in f11:
+    n+=1
+    str = ""
+    if row.find('Answer this question') != -1:
+        continue
+    q1 = row.find('Question:')+9
+    a1 = row.find('Answer:') 
+    qst = name+" "+row[q1:a1]
+    qst = qst.replace('"',"")
+    qst = qst.replace('\\',"")
+    #print(qst)
+    s1 = row.find('</style>') + 8
+    d1 = row.rfind('.')
+    
+    ans = row[s1:d1]
+    ans = ans.replace('"',"")
+    ans = ans.replace("\\","")
+    if ans == "":
+        continue
+    
+    #print(ans)
+    
+    str = "{\n\"type\": \"Faq\",\n\"faq\": {\n\"question\": \""
+    str+= qst+"\",\n\"answer\": \""
+    if n < 49 and n < len(f11)-1:
+        str+= ans+"\"\n}\n},\n"
+        f2.write(str)
+    else:
+        str+= ans+"\"\n}\n}\n"
+        f2.write(str)
+        break
+        
+
+    
+f2.write("\n]")
---
+++ README.txt
@@ -0,0 +1,22 @@
+
+
+//Use this link to access the chatbot service from facebook messenger:
+Facebook Messenger Link : https://m.me/107058280888966?ref=Welcome%20Message
+
+Sample inputs : 
+“Can I play games in my Acer predator helios?”
+“Can I upgrade ram in my acer chromebook?”
+
+For this prototype, we have added FAQ for 10 laptops.
+Please include product name in the query to get specific answers for the product. 
+
+Products supported : 
+Acer aspire 5
+Acer chromebook
+Acer predator helios
+apple macbook air
+Lenovo Ideapad L340
+Lenovo Flex
+Asus chromebook
+HP 2019
+HP 2020
